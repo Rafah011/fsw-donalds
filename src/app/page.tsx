4766b99f43f21c1074b01899cf524a88f1e19b50
Rafah011 <rafah011@hotmail.com>
2025-02-19 11:52:05 -0300

    chore: update css variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
 const HomePage = () => {
   return (
-    <div className="p-5 border-red-500">
+    <div className="p-5 border-red-500 border rounded-xl">
       <h1>Home Page</h1>
+      <Button>Iniciar</Button>
     </div>
   );
 };
